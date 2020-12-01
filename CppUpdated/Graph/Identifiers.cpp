aa140f250b6a699b1f2c7af3fcc2c269b006479b
Antonstockmarr <s164170@student.dtu.dk>
2020-12-01 15:33:10 +0100

    implemented Identifiers

--- CppUpdated/Graph/Identifiers.cpp
+++ CppUpdated/Graph/Identifiers.cpp
@@ -3,14 +3,56 @@
 
 
 #include "Identifiers.h"
-#include "vector"
 
-Identifiers::Identifiers(vector<string> variables, vector<string> arrays, vector<string> records){
-    
-}
 
 Identifiers::Identifiers() {
 }
 
+Identifiers::merge(Identifiers i){
+    Identifiers merged;
+    for (auto var : i.getVariables()){
+        merged.addVariable(var);
+    }
+    for (auto arr : i.getArrays()){
+        merged.addArray(arr);
+    }
+    for (auto rec : i.getRecords()){
+        merged.addRecord(rec);
+    }
+    for (auto var : this->getVariables()){
+        merged.addVariable(var);
+    }
+    for (auto arr : this->getArrays()){
+        merged.addArray(arr);
+    }
+    for (auto rec : this->getRecords()){
+        merged.addRecord(rec);
+    }
+    return merged;
+}
+
+Identifiers::addVariable(string variable) {
+    this->variables.insert(variable);    
+}
+
+Identifiers::addArray(string array) {
+    this->arrays.insert(array);    
+}
+
+Identifiers::addRecord(string record) {
+    this->records.insert(record);
+}
+
+Identifiers::getVariables(){
+    return this->variables;
+}
+
+Identifiers::getArrays(){
+    return this->arrays;
+}
+
+Identifiers::getRecords(){
+    return this->records;
+}
 
 // TODO
--- CppUpdated/Graph/Identifiers.h
+++ CppUpdated/Graph/Identifiers.h
@@ -3,23 +3,26 @@
 #define Identifiers_h
 
 #include <stdio.h>
-#include <vector>
 #include <string>
+#include <unordered_set>
+#include <iostream>
 
 using namespace std;
 
 class Identifiers {
 public:
-    vector<string> variables;
-    vector<string> arrays;
-    vector<string> records;
+    unordered_set<string> variables;
+    unordered_set<string> arrays;
+    unordered_set<string> records;
 
-    Identifiers(vector<string> variables, vector<string> arrays, vector<string> records);
     Identifiers();
     Identifiers merge(Identifiers identifiers);
     void addVariable(string x);
     void addArray(string A);
     void addRecord(string R);
+    unordered_set<string> getVariables();
+    unordered_set<string> getArrays();
+    unordered_set<string> getRecords();
 };
 
 #endif /* Identifiers_h */
